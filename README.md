c78a64f62af5ee63cb6d748a865331b46a98b9ac
“wyc” <2876949683@qq.com>
2023-10-07 17:08:17 +0800

    更新了查询和根据id查询文件

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ web后端采用go
 
 #### 每个服务器部署之前，在models里更改Node名称
 
-创建申请createApply
-127.0.0.1:8080/browse/apply
-{"applyOwner":"C","fileOwner":"A","timestamp":"20220926","fileID":2,"status":0}
+##### postman调试：
 初始化查看我的页面
 127.0.0.1:8080/myfile
-test2
+
+上传文件upload
+127.0.0.1:8080/upload/confirm
+{"id":"","name":"file1","fileOwner":"A","description":"this is file1","size":"1kb","time":"B","status":0}
+
+创建申请createApply
+127.0.0.1:8080/browse/apply
+{"id":"","name":"file1","fileOwner":"A","description":"this is file1","size":"1kb","time":"B","status":0}
+
+根据id查询单个文件
+127.0.0.1:8080/myfile/onefile/A16966616127948220009928
--- models/todo.go
+++ models/todo.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"math/rand"
 	"net/http"
-	"path"
 	"strconv"
 	"time"
 )
@@ -25,7 +24,7 @@ type Apply struct {
 	ApplyOwner string `json:"applyOwner"`
 	FileOwner  string `json:"fileOwner"`
 	Time       string `json:"time"`
-	FileID     string `json:"fileID"`
+	FileID     string `json:"id"`
 	Status     int    `json:"status"`
 }
 
@@ -36,18 +35,18 @@ var Node string = "A" //节点
 */
 
 func UploadFiles(file *File, context *gin.Context) (err error) {
-	f, err := context.FormFile("f1")
+	//f, err := context.FormFile("f1")
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 	} else {
 		//保存读取的文件到本地服务器
-		dst := path.Join("./", f.Filename)
-		_ = context.SaveUploadedFile(f, dst)
-		context.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-		})
+		//dst := path.Join("./", f.Filename)
+		//_ = context.SaveUploadedFile(f, dst)
+		//context.JSON(http.StatusOK, gin.H{
+		//	"status": "ok",
+		//})
 		//生成文件ID
 		t := time.Now()
 		file.Time = t.Format("2006-01-02 15:04:05")
@@ -62,7 +61,7 @@ func UploadFiles(file *File, context *gin.Context) (err error) {
 	return
 }
 
-func CreateAplly(file *File) (err error) {
+func CreateApply(file *File) (err error) {
 	var apply Apply
 	apply.ApplyOwner = Node
 	apply.FileOwner = file.FileOwner
@@ -76,31 +75,31 @@ func CreateAplly(file *File) (err error) {
 	return
 }
 
-func GetFileList() (filelist []*File, err error) {
-	if err = dao.DB.Find(&filelist).Error; err != nil {
+func GetFileList() (fileList []*File, err error) {
+	if err = dao.DB.Where("file_owner = ?", Node).Find(&fileList).Error; err != nil {
 		return nil, err
 	}
 	return
 }
 
-func GetMyApply(node string) (applylist []*Apply, err error) {
-	if err = dao.DB.Where("apply_owner <> ?", node).Find(&applylist).Error; err != nil {
+func GetMyApply() (applyList []*Apply, err error) {
+	if err = dao.DB.Where("apply_owner = ?", Node).Find(&applyList).Error; err != nil {
 		return nil, err
 	}
 	return
 }
 
 // 连接apply数据库表
-func GetApplyList(node string) (applylist []*Apply, err error) {
-	if err = dao.DB.Where("file_owner <> ?", node).Find(&applylist).Error; err != nil {
+func GetApplyList() (applyList []*Apply, err error) {
+	if err = dao.DB.Where("file_owner = ?", Node).Find(&applyList).Error; err != nil {
 		return nil, err
 	}
 	return
 }
 
-func GetTodoByID(id string) (todo *File, err error) {
-	todo = new(File)
-	if err = dao.DB.Where("id=?", id).First(&todo).Error; err != nil {
+func GetFileByID(id string) (file *File, err error) {
+	file = new(File)
+	if err = dao.DB.Where("file_id = ?", id).First(&file).Error; err != nil {
 		return nil, err
 	}
 	return
